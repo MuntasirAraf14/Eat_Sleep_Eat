57fdc5425ac4a544afe430c12b7dff223775c11d
MuntasirAraf <112501683+MuntasirAraf@users.noreply.github.com>
2024-11-26 14:04:54 +0600

    FUN

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const addEventOnElements = function(elements, eventType, callback){
+    for(let i=0, len = elements.length; i<len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
